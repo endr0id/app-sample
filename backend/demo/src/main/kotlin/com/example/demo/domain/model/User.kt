c5e269d5fa792764640b12c67edfbc79dc3ce6f1
w4.3nd0 <w4.3nd0@gmail.com>
2025-08-30 01:24:40 +0900

    feat: projectを入れ直し

--- backend/demo/src/main/kotlin/com/example/demo/domain/model/User.kt
+++ backend/demo/src/main/kotlin/com/example/demo/domain/model/User.kt
@@ -1,3 +1,17 @@
 package com.example.demo.domain.model
 
-data class User()
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "user")
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false)
+    val name: String,
+
+    @Column(nullable = false, unique = true)
+    val email: String
+)

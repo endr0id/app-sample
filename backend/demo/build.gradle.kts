708347284e34359ae7856a78fa82b91bc3dd48e1
w4.3nd0 <w4.3nd0@gmail.com>
2025-09-14 01:55:12 +0900

    chore: ライブラリ追加 (#11)

--- backend/demo/build.gradle.kts
+++ backend/demo/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    // implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")

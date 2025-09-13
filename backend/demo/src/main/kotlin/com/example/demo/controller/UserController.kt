6304488867874ffc6de9b275b787c43a1ed7f523
w4.3nd0 <w4.3nd0@gmail.com>
2025-09-13 20:10:53 +0900

    feat: 単体User取得処理変更
    対応内容:
    - クエリパラメータに負の数 or 0を入れれない様に変更

--- backend/demo/src/main/kotlin/com/example/demo/controller/UserController.kt
+++ backend/demo/src/main/kotlin/com/example/demo/controller/UserController.kt
@@ -1,16 +1,17 @@
 package com.example.demo.controller
 
 import com.example.demo.application.dto.ApiResponse
-import com.example.demo.application.services.UserService
 import com.example.demo.application.dto.response.user.UserResponseDTO
 import com.example.demo.application.mapper.ApiResponseMapper
 import com.example.demo.application.mapper.toResponseDTO
 import com.example.demo.application.mapper.toResponseDTOList
+import com.example.demo.application.services.UserService
+import jakarta.validation.constraints.Min
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.PathVariable
 
 @RestController
 @RequestMapping("/users")
@@ -24,7 +25,7 @@ class UserController(
     }
 
     @GetMapping("/{id}")
-    fun getUser(@PathVariable id: Long): ResponseEntity<ApiResponse<UserResponseDTO>> {
+    fun getUser(@PathVariable @Min(1) id: Long): ResponseEntity<ApiResponse<UserResponseDTO>> {
         val user = userService.getUser(id)?.toResponseDTO()
         return user?.let { ResponseEntity.ok(ApiResponseMapper.wrap(user)) } ?: ResponseEntity.notFound().build()
     }

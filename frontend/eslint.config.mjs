4f73dbfe724fff9974981018062fa8202cb34baa
w4.3nd0 <w4.3nd0@gmail.com>
2025-09-21 23:30:05 +0900

    chore: ESLintとprettier設定を追加 (#17)

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // WARN:
+  // extends内は後述であるほど設定が優先されprettierはeslintの設定と競合しない様、最後に記述
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "next",
+    "prettier",
+  ),
   {
     ignores: [
       "node_modules/**",
@@ -19,6 +27,13 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
   },
 ];
 

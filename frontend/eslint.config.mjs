d96b5f45bf5c4d97b7b6fbe0ec851f42b808108e
w4.3nd0 <w4.3nd0@gmail.com>
2025-09-26 00:04:40 +0900

    fix: Lint configが正しく動作していないため、リセット (#17)
    対応内容
    - 設定削除
    - paserにtypescript-parserを追加

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,27 +1,16 @@
-import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      "plugin:react/recommended",
-      "plugin:prettier/recommended",
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
-    plugins: ["@typescript-eslint", "react", "react-hooks", "react-refresh"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: "@typescript-eslint/parser",
+      parser: tsParser,
+      sourceType: "module",
     },
   },
 ]);

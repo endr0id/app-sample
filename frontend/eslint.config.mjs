d7a04a48b6424c836d9a86f6c0f6a8ff43cbfa01
w4.3nd0 <w4.3nd0@gmail.com>
2025-09-26 00:11:38 +0900

    feat: jsのlint追加 (#17)

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,3 +1,4 @@
+import js from "@eslint/js";
 import tsParser from "@typescript-eslint/parser";
 import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
@@ -6,6 +7,7 @@ export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
+    extends: [js.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,

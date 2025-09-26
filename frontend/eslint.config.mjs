7536643318405c1fcf5427064454a2d620401ea8
w4.3nd0 <w4.3nd0@gmail.com>
2025-09-26 22:57:13 +0900

    feat: #8580 不要なimportはeslint --fix時に消す設定 (#17)

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import tsParser from "@typescript-eslint/parser";
 import { defineConfig, globalIgnores } from "eslint/config";
 import importPlugin from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -12,6 +13,7 @@ export default defineConfig([
     extends: [js.configs.recommended, tseslint.configs.recommended],
     plugins: {
       import: importPlugin,
+      "unused-imports": unusedImports,
     },
     languageOptions: {
       ecmaVersion: 2020,
@@ -50,6 +52,17 @@ export default defineConfig([
       "@typescript-eslint/consistent-type-imports": [
         "error",
         { prefer: "type-imports" },
+      ],
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
       ],
     },
   },

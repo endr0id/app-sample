489951d06444913c849e43055b324b2e6084072f
w4.3nd0 <w4.3nd0@gmail.com>
2025-10-05 01:39:52 +0900

    feat: routing追加 (#22)

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,12 +1,28 @@
+import { useMemo } from "react";
+
 import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
 import App from "../App";
+import { paths } from "../config/paths";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+// TODO: loader追加検討
+const createAppRouter = () =>
+  createBrowserRouter([
+    {
+      path: paths.home.path,
+      element: <App />,
+    },
+    {
+      path: paths.auth.path,
+      element: <>login</>,
+    },
+  ]);
 
-export default router;
+const AppRouter = () => {
+  const router = useMemo(() => createAppRouter(), []);
+
+  return <RouterProvider router={router} />;
+};
+
+export default AppRouter;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router/dom";
 
 import "./index.css";
-import router from "./app/router";
+import AppRouter from "./app/router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AppRouter />
   </StrictMode>,
 );

378f32452c329f9db7f2958719b60df77df3e829
w4.3nd0 <w4.3nd0@gmail.com>
2025-10-11 17:55:06 +0900

    chore: storybookのレイアウト関連は別階層で管理 (#22)

---
+++ frontend/src/stories/layouts/auth/login-layout.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import AuthLayout from "../../../components/layouts/auth/AuthLayout";
+import Button from "../../../components/ui/button/Button";
+import Form from "../../../components/ui/form/Form";
+import Input from "../../../components/ui/form/input/Input";
+
+const meta = {
+  title: "Layout/Auth/Login",
+  component: AuthLayout,
+  args: {
+    children: (
+      <Form>
+        <Input />
+        <Input />
+        <Button>Login</Button>
+      </Form>
+    ),
+    title: "Login Layout",
+  },
+} satisfies Meta<typeof AuthLayout>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Login: Story = {
+  render: (args) => <AuthLayout title={args.title}>{args.children}</AuthLayout>,
+};
